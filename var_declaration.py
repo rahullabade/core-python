a924d5b0508c4437af803bba0d8156f49ed487b3
rahullabade <rahullabade05@gmail.com>
2024-06-27 20:06:06 +0530

    basic type and add program

---
+++ var_declaration.py
@@ -0,0 +1,17 @@
+num1 = 20
+num2 = 30 
+name = "Rahul"
+status = True
+print(type(num1))
+print(type(name))
+print(type(status))
+print(num1)
+print(num2)
+print(name)
+print(status)
+sum = num1 + num2
+print(sum)
+#
+"""
+stdent so tired
+"""
